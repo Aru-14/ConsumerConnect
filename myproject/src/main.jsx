7f52eb99f015865399bff0b15b6d555e814f7f59
Arunima Paunikar <147681796+Aru-14@users.noreply.github.com>
2025-07-21 21:57:42 +0530

    main.jsx

--- myproject/src/main.jsx
+++ myproject/src/main.jsx
@@ -1,13 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 import { ProductProvider } from './context/ProductProvider.jsx';
 import { CartProvider } from './context/CartProvider';
 import { AuthProvider } from './context/AuthProvider.jsx';
@@ -17,7 +8,7 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 
-import './index.css'; // if you’re using Tailwind or custom styles
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -25,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <CartProvider>
 <ProductProvider>
-     {/* 🎒 Wrapping your app with the backpack */}
+ 
       <App />
     
     </ProductProvider>
